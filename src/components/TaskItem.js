c15e97eea1d0798e9dc121660fd66b86498ee447
PratikGarai <pratikgarai0208@gmail.com>
2020-09-03 04:21:47 +0530

    Added the taskitem skeleton

---
+++ src/components/TaskItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TaskItem extends React.Component{
+
+	render(){
+		return(
+			<input type = "checkbox" checked = "true">CheckBox</input>
+		);
+	}
+}
+
+export default TaskItem;
