2c1b7b2159bb487465acebfd44be14d02fa256a1
PratikGarai <pratikgarai0208@gmail.com>
2020-09-03 13:56:46 +0530

    Added the event handler for checking and unchecking

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -5,7 +5,7 @@ class TaskItem extends React.Component{
 	render(){
 		return(
 			<div className="task-list-item">
-				<input type = "checkbox" checked = {this.props.item.done} />
+				<input type = "checkbox" checked = {this.props.item.done} onChange={()=> this.props.changeHandler(this.props.item.id)} />
 			 	<span className="task-content">{this.props.item.task}</span>
 				<br /><hr />
 			</div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,8 +3,32 @@ import TaskItem from './TaskItem.js';
 import Tasks from './Tasks.json';
 
 class TaskList extends React.Component {
+
+	constructor(){
+		super();
+		this.state = {
+			tasksList : Tasks.tasks
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(id){
+		console.log(id);
+		this.setState(prevState => {
+			const NewList = prevState.tasksList.map(item => {
+				if (item.id===id){
+					item.done = !item.done;
+				}
+				return item;
+			});
+			return {
+				tasksList : NewList
+			}
+		});
+	}
+	
 	render(){
-        	const CompList = Tasks.tasks.map(item => <TaskItem key={item.id} item={item} />);
+        	const CompList = this.state.tasksList.map(item => <TaskItem key={item.id} item={item} changeHandler={this.handleChange}/>);
 		return(
 			<div className="task-list-div">
 				{CompList}
