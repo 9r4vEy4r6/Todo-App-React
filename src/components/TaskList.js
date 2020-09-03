1f6a528519cf1f45f9f10c73979a9880af6ae53e
PratikGarai <pratikgarai0208@gmail.com>
2020-09-03 10:02:58 +0530

    Added ids

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import TaskItem from './TaskItem.js';
+import Tasks from './Tasks.json';
 
 class TaskList extends React.Component {
-
+        const CompList = Tasks.tasks.map<
 	render(){
 		return(
 			<div className="task-list-div">
